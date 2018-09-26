c778617e1e18259f04872d3440e8cea5b0e31ade
andy <andy@DESKTOP-K86GSGE>
2018-09-26 09:43:03 +0800

    添加项目文件。

---
+++ WebApplication2/wwwroot/js/main.js
@@ -0,0 +1,13 @@
+﻿import '@progress/kendo-ui'
+import '@progress/kendo-theme-default/dist/all.css'
+
+import { DropDownTree, DropDownTreeInstaller } from '@progress/kendo-dropdowntree-vue-wrapper'
+
+Vue.use(DropDownTreeInstaller)
+
+new Vue({
+    el: '#app',
+    components: {
+        DropDownTree
+    }
+})
